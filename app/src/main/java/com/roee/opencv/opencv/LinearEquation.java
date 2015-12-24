26fb6fff5bab44b24f8c2009a984aea591f38a6a
Roee Hendel <roee97@gmail.com>
2015-12-24 20:35:47 +0200

    Add LinearEquation class

---
+++ app/src/main/java/com/roee/opencv/opencv/LinearEquation.java
@@ -0,0 +1,30 @@
+package com.roee.opencv.opencv;
+
+/**
+ * Created by Roee on 24/12/2015.
+ */
+public class LinearEquation {
+    private double a;
+    private double b;
+
+    public LinearEquation(double a, double b){
+        this.a = a;
+        this.b = b;
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public void setA(double a) {
+        this.a = a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
+}
